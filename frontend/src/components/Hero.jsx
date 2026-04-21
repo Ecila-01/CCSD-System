287c68e4cb481ecd5665cbe1a87ee4a761cd3853
wipreymmm <20227315@s.ubaguio.edu>
2026-04-21 22:55:50 +0800

    deployment

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import "../styles/Hero.css";
+import ccsdLogo from "../assets/ccsdLogo.png"; 
 
 function Hero({ background, title, subtitle }) {
   return (
@@ -9,11 +10,15 @@ function Hero({ background, title, subtitle }) {
       <div className="heroOverlay">
         <div className="heroContent">
           
-          <h1 className="heroMainTitle"><img
-            src="src/assets/ccsdLogo.png"
-            alt="CCSD Logo"
-            className="heroLogo"
-          />{title}</h1>
+          <h1 className="heroMainTitle">
+            <img
+              src={ccsdLogo} 
+              alt="CCSD Logo"
+              className="heroLogo"
+            />
+            {title}
+          </h1>
+          
           {subtitle && <h2 className="heroSubTitle">{subtitle}</h2>}
         </div>
       </div>
